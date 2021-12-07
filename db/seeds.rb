86aaecd973410fd14ff15614d8012a43d186f12d
Sarah <sarahlchavez@gmail.com>
2021-12-07 14:27:18 -0800

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Score.create([
+    {username: "sarah", time: "01:23"},
+    {username: "elmo", time: "01:15"},
+    {username: "ernie", time: "01:33"}
+])
