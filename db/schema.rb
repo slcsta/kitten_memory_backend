6273b15a3e7fa224f864a7c6e36b9a78b336b852
Sarah <sarahlchavez@gmail.com>
2021-12-22 05:07:35 -0800

    remigrated db with updated turns

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2021_12_07_214822) do
 
   create_table "scores", force: :cascade do |t|
     t.string "username"
-    t.string "time"
+    t.string "turns"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
